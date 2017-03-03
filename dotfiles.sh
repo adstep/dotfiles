330cb6647dc8c3bff69ade6266a1452160620a1e
root <root@ADSTEP-DEV.redmond.corp.microsoft.com>
2017-03-03 02:30:49 -0800

    first commit

---
+++ dotfiles.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+/bin/cp -rf ~/.bashrc bash/.bashrc
+/bin/cp -rf ~/.tmux.conf tmux/.tmux.conf
+/bin/cp -rf ~/.vimrc vim/.vimrc
+/bin/cp -rf ~/.zshrc zsh/.zshrc
+
---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+/bin/cp -rf bash/.bashrc ~/.bashrc
+/bin/cp -rf tmux/.tmux.conf ~/.tmux.conf
+/bin/cp -rf vim/.vimrc ~/.vimrc
+/bin/cp -rf zsh/.zshrc ~/.zshrc
