e47079de88c3592e85b2e3d64ff36d3ce108520a
Adam Stephenson <adstep@microsoft.com>
2018-11-10 23:52:53 -0800

    remove wsl related config

--- install.sh
+++ install.sh
@@ -12,10 +12,6 @@ sudo apt-get install -y zsh
 # Vundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
-# NodeJS
-curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
-sudo apt-get install -y nodejs
-
 # NPM Packages
 sudo npm i -g typescript http-server
 
@@ -25,22 +21,6 @@ git config --global push.default matching
 
 # Zsh Syntax Highlighting
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/zsh-syntax-highlighting
-
-# Build Essential
-sudo apt-get install -y build-essential
-
-# Make
-sudo apt-get install -y make
-
-# GDB
-sudo apt-get install -y gdb
-
-# WSL-Terminal
-wget https://github.com/goreliu/wsl-terminal/releases/download/v0.8.11/wsl-terminal-0.8.11.7z
-7z x wsl-terminal-0.8.11.7z
-cp wsl-terminal /mnt/c/wsl-terminal/
-rm -rf wsl-terminal-0.8.11.7z
-cp schemes/afterglow.minttyrc /mnt/c/dev/wsl-terminal/etc/themes/afterglow.minttyrc
 
 # Bash-It Install
 git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it
