b8d940ce8015bbef907c99c14c27ebc57df7b805
adstep <stephensonadamj@gmail.com>
2023-03-05 04:40:43 -0800

    fix cloning codedark theme

--- setup.sh
+++ setup.sh
@@ -6,9 +6,18 @@ cp ./dircolors/.dircolors  ~/.dircolors
 cp -r ./oh-my-posh/        ~/.oh-my-posh/
 cp ./tmux/.tmux.conf       ~/.tmux.conf
 cp ./tmux/.tmux.conf.local ~/.tmux.conf.local
-cp vim/.vimrc               ~/.vimrc
+cp vim/.vimrc              ~/.vimrc
 
-mkdir -p ~/.vim/pack/themes/start
+# Install vundle
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+# Download vscode-dark-theme
+rm -rf ~/.vim/pack/themes/start
+mkdir ~/.vim/pack/themes/start
 git clone https://github.com/tomasiser/vim-code-dark ~/.vim/pack/themes/start
 
+# Ensure vim plugins are installed via Vundle
 vim +PluginInstall +qall
+
+# Install oh-my-posh
+brew install jandedobbeleer/oh-my-posh/oh-my-posh
