7e8ef6a76c1a7a1a2ec62fb939d90d5053fe48ca
Adam Stephenson <adstep@microsoft.com>
2023-03-06 12:19:53 -0800

    include hostname in prompt theme

--- setup.sh
+++ setup.sh
@@ -3,10 +3,12 @@
 cp ./bash/.bash_aliases    ~/.bash_aliases
 cp ./bash/.bashrc          ~/.bashrc
 cp ./dircolors/.dircolors  ~/.dircolors
-cp -r ./oh-my-posh/        ~/.oh-my-posh/
+cp -r ./oh-my-posh/themes/ ~/.oh-my-posh/
 cp ./tmux/.tmux.conf       ~/.tmux.conf
 cp ./tmux/.tmux.conf.local ~/.tmux.conf.local
 cp vim/.vimrc              ~/.vimrc
 
 # Ensure vim plugins are installed via Vundle
 vim +PlugInstall +qall
+
+source ~/.bashrc
